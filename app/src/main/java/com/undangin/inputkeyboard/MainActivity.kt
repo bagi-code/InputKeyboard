afdead23d4c9209382467ca397c6b5f1005380b6
bagicode <bagicode@gmail.com>
2019-04-23 16:17:19 +0700

    function keyboard delete

--- app/src/main/java/com/undangin/inputkeyboard/MainActivity.kt
+++ app/src/main/java/com/undangin/inputkeyboard/MainActivity.kt
@@ -2,7 +2,6 @@ package com.undangin.inputkeyboard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,41 +15,79 @@ class MainActivity : AppCompatActivity() {
         inputKeyboard = ""
 
         button.setOnClickListener {
-            inputKeyboard +="1"
-
-            showValue(inputKeyboard)
+            if (inputKeyboard.length < 3) {
+                inputKeyboard += "1"
+                showValue(inputKeyboard)
+            }
         }
+
         button2.setOnClickListener {
-            inputKeyboard +="2"
-
-            showValue(inputKeyboard)
+            if (inputKeyboard.length < 3) {
+                inputKeyboard += "2"
+                showValue(inputKeyboard)
+            }
         }
-        button3.setOnClickListener {
-            inputKeyboard +="3"
 
-            showValue(inputKeyboard)
+        button3.setOnClickListener {
+            if (inputKeyboard.length < 3) {
+                inputKeyboard += "3"
+                showValue(inputKeyboard)
+            }
+        }
+
+        button4.setOnClickListener {
+            if (inputKeyboard.length < 3) {
+                inputKeyboard += "5"
+                showValue(inputKeyboard)
+            }
+        }
+
+        button5.setOnClickListener {
+            if (inputKeyboard.length < 3) {
+                inputKeyboard += "4"
+                showValue(inputKeyboard)
+            }
+        }
+
+        button6.setOnClickListener {
+            if (inputKeyboard.length > 0) {
+
+                delValue(inputKeyboard)
+            }
         }
     }
 
     fun showValue(inputKeyboardDatas:String) {
-
         if (inputKeyboardDatas.length == 1) {
             et_1.setText(inputKeyboardDatas.substring(0,1))
-
-        } else if (inputKeyboardDatas.length == 2) {
-            et_1.setText(inputKeyboardDatas.substring(0,1))
-            et_2.setText(inputKeyboardDatas.substring(1,inputKeyboardDatas.length))
             et_2.requestFocus()
 
-        } else if (inputKeyboardDatas.length == 3) {
-            et_1.setText(inputKeyboardDatas.substring(0,1))
-            et_2.setText(inputKeyboardDatas.substring(1,2))
-            et_3.setText(inputKeyboardDatas.substring(2,inputKeyboardDatas.length))
+        } else if (inputKeyboardDatas.length == 2) {
+            et_2.setText(inputKeyboardDatas.substring(1,inputKeyboardDatas.length))
             et_3.requestFocus()
 
-        } else {
+        } else if (inputKeyboardDatas.length == 3) {
+            et_3.setText(inputKeyboardDatas.substring(2,inputKeyboardDatas.length))
 
         }
+
+    }
+
+    fun delValue(inputKeyboardDatas:String) {
+        if (inputKeyboardDatas.length == 1) {
+            et_1.setText("")
+
+        } else if (inputKeyboardDatas.length == 2) {
+            et_2.setText("")
+            et_1.requestFocus()
+
+        } else if (inputKeyboardDatas.length == 3) {
+            et_3.setText("")
+            et_2.requestFocus()
+
+        }
+
+        inputKeyboard = inputKeyboard.substring(0, inputKeyboard.length - 1)
 
     }
 }
